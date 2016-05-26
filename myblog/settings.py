620524e5e4414dd11f3f6c8a9cb35c1106071ba4
calvin <yanran200830@163.com>
2016-05-26 09:45:19 +0800

    多说

--- myblog/settings.py
+++ myblog/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "media"),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')  
-MEDIA_URL = '/media/'  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+MEDIA_URL = '/media/'
 
 PAGINATION_DEFAULT_WINDOW  = 2 #分页设置
