087e94c9f7aa0bd7f6900a0915b876a97714e118
yanran <yanran200830@163.com>
2016-07-07 20:58:45 +0800

    111

--- wechat/urls.py
+++ wechat/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^wechat/$',views.index),	
+    url(r'^wechat/$',views.wechat),
+    url(r'^index/$',views.index),
+    url(r'^demo1/$',views.demo1),
+    url(r'^demo2/$',views.demo2)	
 ]
--- wechat/views.py
+++ wechat/views.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from wechat_sdk import WechatBasic
 from wechat_sdk.exceptions import ParseError
@@ -17,7 +18,8 @@ wechat_instance = WechatBasic(
     appsecret=AppSecret
 )
 
-def index(request):
+
+def wechat(request):
     if request.method == 'GET':
         # 检验合法性
         # 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
@@ -32,7 +34,6 @@ def index(request):
         return HttpResponse(
             request.GET.get('echostr', ''), content_type="text/plain")
  
- 
     # 解析本次请求的 XML 数据
     try:
         wechat_instance.parse_data(data=request.body)
@@ -41,29 +42,42 @@ def index(request):
  
     # 获取解析好的微信请求信息
     message = wechat_instance.get_message()
-    print(message)
  
     # 关注事件以及不匹配时的默认回复
     response = wechat_instance.response_text(
         content = (
             '感谢您的关注！\n回复【功能】两个字查看支持的功能，还可以回复任意内容开始聊天'
-            '\n【<a href="http://www.ziqiangxuetang.com">自强学堂手机版</a>】'
+            '\n【<a href="http://104.128.80.53:8000/">小心的博客</a>】'
             ))
-    # if isinstance(message, TextMessage):
-    #     # 当前会话内容
-    #     content = message.content.strip()
-    #     if content == '功能':
-    #         reply_text = (
-    #                 '目前支持的功能：\n1. 关键词后面加上【教程】两个字可以搜索教程，'
-    #                 '比如回复 "Django 后台教程"\n'
-    #                 '2. 回复任意词语，查天气，陪聊天，讲故事，无所不能！\n'
-    #                 '还有更多功能正在开发中哦 ^_^\n'
-    #                 '【<a href="http://www.ziqiangxuetang.com">自强学堂手机版</a>】'
-    #             )
-    #     elif content.endswith('教程'):
-    #         reply_text = '您要找的教程如下：'
+    if isinstance(message, TextMessage):
+        # 当前会话内容
+        content = message.content.strip()
+        if content == '功能':
+            reply_text = (
+                    '目前支持的功能：\n1. 关键词后面加上【教程】两个字可以搜索教程，'
+                    '比如回复 "Django 后台教程"\n'
+                    '2. 回复任意词语，查天气，陪聊天，讲故事，无所不能！\n'
+                    '还有更多功能正在开发中哦 ^_^\n'
+                    '【<a href="http://104.128.80.53:8000/">小心的博客</a>】'
+                )
+        elif content.endswith('教程'):
+            reply_text = '您要找的教程如下：'
+        elif content == '贰颜':
+            reply_text = '她是猪 ^ _ ^'
+        else:
+            reply_text = (
+                '自己玩泥巴去吧 ^ _ ^\n'
+                '【<a href="http://6a071219.ngrok.natapp.cn/demo2/">小心的博客</a>】')
  
-    #     response = wechat_instance.response_text(content=reply_text)
+        response = wechat_instance.response_text(content=reply_text)
  
     return HttpResponse(response, content_type="application/xml")
 
+def index(request):
+	return render(request,"wechat/index.html")
+
+def demo1(request):
+	return render(request,"wechat/demo1.html") 
+
+def demo2(request):
+	return render(request,"wechat/demo2.html") 
