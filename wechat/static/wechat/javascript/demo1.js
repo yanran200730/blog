24712db47ec450f5bea8b265ce710546e1b4d319
calvin <yanran200830@163.com>
2016-07-12 19:52:47 +0800

    111

--- wechat/static/wechat/javascript/demo1.js
+++ wechat/static/wechat/javascript/demo1.js
@@ -1,6 +1,40 @@
-$(document).ready(function(){
-	$("#btn").on("click",function(){
-		alert($("body").width())
-	})
-	console.log($("body").width());
-});
+var myapp = angular.module('myapp', ['ngAnimate']);
+
+myapp.controller('myctrl', ['$scope', function($scope){
+	$scope.hide = "hidden";
+	$scope.show = "active"; 
+	$scope.mycheck = false;
+	$scope.str = "str";
+	$scope.change = function(){
+		console.log($scope.mycheck)
+		if ($scope.mycheck){
+			$scope.mycheck = false;
+			console.log($scope.mycheck)
+		}else{
+			$scope.mycheck = true;			
+		}
+		console.log($scope.mycheck)
+	}
+}])
+
+
+// myapp.directive('change', ['', function(){
+// 	// Runs during compile
+// 	return {
+// 		// name: '',
+// 		// priority: 1,
+// 		// terminal: true,
+// 		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+// 		// controller: function($scope, $element, $attrs, $transclude) {},
+// 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+// 		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+// 		// template: '',
+// 		// templateUrl: '',
+// 		// replace: true,
+// 		// transclude: true,
+// 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+// 		link: function($scope, element,, attrs) {
+			
+// 		}
+// 	};
+// }]);
